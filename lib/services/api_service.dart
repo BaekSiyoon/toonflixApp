363177a51139c0b16e53850c08331be3c7adeb1d
BaekSiyoon <tksemf2543@naver.com>
2024-08-14 03:54:27 +0900

    받아온 데이터를 JSON 형식으로 변경 후 리스트에 담기

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -6,12 +9,18 @@ class ApiService {
 
   // await 를 사용하려면 async(비동기)함수 내에서 써야한다.
   // await는 dart한테 결과 값이 올때까지기다리라고 할때 사용 (비동기)
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      // 받아온 데이터를 JSON 형식으로 변경
+      // jsonDecode의 반환값은 dynamic 이다
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
